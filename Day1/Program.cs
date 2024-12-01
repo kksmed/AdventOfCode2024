2a4fcb0c0ee370ceea9a7131aa6aae3bd19b2c45
Kristoffer Smed <mail@kristoffersmed.dk>
2024-12-01 08:03:50 +0100

    Parse input

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,3 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿var example =
+  """
+  3   4
+  4   3
+  2   5
+  1   3
+  3   9
+  3   3
+  """;
 
-Console.WriteLine("Hello, World!");
+var input = example.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+var (a, b) = Parse(input);
+
+
+Console.WriteLine($"{a} {b}");
+
+return;
+
+(List<int> a, List<int> b) Parse(string[] input)
+{
+  var a = new List<int>();
+  var b = new List<int>();
+  foreach (var line in input)
+  {
+    var parts = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    if (parts.Length != 2)
+    {
+      throw new ArgumentException("Invalid input");
+    }
+
+    a.Add(int.Parse(parts[0]));
+    b.Add(int.Parse(parts[1]));
+  }
+
+  return (a, b);
+}
