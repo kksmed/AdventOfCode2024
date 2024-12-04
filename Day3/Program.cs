dc91aabfc3800f4ded7904a525e4a218dcd8de2c
Kristoffer Smed <mail@kristoffersmed.dk>
2024-12-04 20:42:41 +0100

    Day 3: Part 2

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -9,3 +9,8 @@ var example = """
 Solver solver = new();
 Solving.SolveExample(solver, example);
 Solving.Solve(solver);
+
+Console.WriteLine("### PART 2 ###");
+Solver2 solver2 = new();
+Solving.SolveExample(solver2, "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))");
+Solving.Solve(solver2);
---
+++ Day3/Solver2.cs
@@ -0,0 +1,59 @@
+using System.Text.RegularExpressions;
+
+using Common;
+
+namespace Day3;
+
+public partial class Solver2 : ISolver<IEnumerable<IInstruction>>
+{
+  readonly Regex regex = MyRegex();
+
+  public IEnumerable<IInstruction> Parse(string[] input) =>
+    input.SelectMany(x => regex.Matches(x))
+      .Select(
+        match => match.Groups[0].Value switch
+          {
+            "do()" => (IInstruction)new Do(),
+            "don't()" => new Dont(),
+            var mul when mul.StartsWith("mul", StringComparison.InvariantCulture) => new Mul(
+              int.Parse(match.Groups[2].Value),
+              int.Parse(match.Groups[3].Value)),
+            _ => throw new ArgumentOutOfRangeException(nameof(input), $"Unknown instruction: {match.Groups[0].Value}")
+          });
+
+  public int SolveFirst(IEnumerable<IInstruction> data)
+  {
+    var sum = 0;
+    var doMul = true;
+    foreach (var instruction in data)
+    {
+      switch (instruction)
+      {
+        case Do _:
+          doMul = true;
+          break;
+        case Dont _:
+          doMul = false;
+          break;
+        case Mul mul when doMul:
+          sum += mul.A * mul.B;
+          break;
+        case Mul mul when !doMul:
+          continue;
+        default:
+          throw new ArgumentOutOfRangeException(nameof(data), $"Unknown instruction: {instruction}");
+      }
+    }
+    return sum;
+  }
+
+  public int? SolveSecond(IEnumerable<IInstruction> data) => null;
+
+  [GeneratedRegex(@"(mul\((\d+),(\d+)\))|(do\(\))|(don't\(\))")]
+  private static partial Regex MyRegex();
+}
+
+public interface IInstruction { }
+record Mul(int A, int B) : IInstruction;
+record Do() : IInstruction;
+record Dont() : IInstruction;
