2fb16e607c216b732e54ae66b78c8394a64d3b49
Kristoffer Smed <mail@kristoffersmed.dk>
2024-12-04 20:22:51 +0100

    Day 3: Part 1

---
+++ Day3/Program.cs
@@ -0,0 +1,11 @@
+﻿using Common;
+
+using Day3;
+
+var example = """
+              xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+              """;
+
+Solver solver = new();
+Solving.SolveExample(solver, example);
+Solving.Solve(solver);
---
+++ Day3/Solver.cs
@@ -0,0 +1,21 @@
+using System.Text.RegularExpressions;
+
+using Common;
+
+namespace Day3;
+
+public partial class Solver : ISolver<IEnumerable<(int, int)>>
+{
+  readonly Regex regex = MyRegex();
+
+  public IEnumerable<(int, int)> Parse(string[] input) =>
+    input.SelectMany(x => regex.Matches(x))
+      .Select(x => (int.Parse(x.Groups[1].Value), int.Parse(x.Groups[2].Value)));
+
+  public int SolveFirst(IEnumerable<(int, int)> data) => data.Sum(x => x.Item1 * x.Item2);
+
+  public int? SolveSecond(IEnumerable<(int, int)> data) => null;
+
+  [GeneratedRegex(@"mul\((\d+),(\d+)\)")]
+  private static partial Regex MyRegex();
+}
