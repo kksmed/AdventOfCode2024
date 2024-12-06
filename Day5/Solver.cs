ebbd2f93e42243ea8128615fe46cbbce07f0bbcd
Kristoffer Smed <mail@kristoffersmed.dk>
2024-12-06 20:57:53 +0100

    Day 5 init

---
+++ Day5/Solver.cs
@@ -0,0 +1,21 @@
+using Common;
+
+namespace Day5;
+
+public class Solver : ISolver<char[,]>
+{
+  public char[,] Parse(string[] input)
+  {
+    throw new NotImplementedException();
+  }
+
+  public int SolveFirst(char[,] data)
+  {
+    throw new NotImplementedException();
+  }
+
+  public int? SolveSecond(char[,] data)
+  {
+    throw new NotImplementedException();
+  }
+}
