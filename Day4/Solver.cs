aefdd688bfda1417735820ea1029a8ca15e78eaa
Kristoffer Smed <mail@kristoffersmed.dk>
2024-12-06 20:53:32 +0100

    Day 4: part 2

--- Day4/Solver.cs
+++ Day4/Solver.cs
@@ -71,6 +71,47 @@ public class Solver : ISolver<char[,]>
 
   public int? SolveSecond(char[,] data)
   {
-    return null;
+    var count = 0;
+    for(var x = 0; x < data.GetLength(0); x++)
+    {
+      for(var y = 0; y < data.GetLength(1); y++)
+      {
+        var c = data[x, y];
+        if (c != 'A') continue;
+
+        var candidates = DetermineCandidates2(new(x, y));
+        var matches = candidates.Count(candidate => CheckCandidate2(data, candidate));
+        count += matches;
+      }
+    }
+
+    return count;
+  }
+
+  static IEnumerable<(Point M1, Point M2, Point S1, Point S2)> DetermineCandidates2(Point p)
+  {
+    var (x, y) = (p.X, p.Y);
+    var topLeft = new Point(x - 1, y - 1);
+    var topRight = new Point(x + 1, y - 1);
+    var bottomLeft = new Point(x - 1, y + 1);
+    var bottomRight = new Point(x + 1, y + 1);
+    // Downward
+    yield return (topLeft, topRight, bottomLeft, bottomRight);
+    // Upward
+    yield return (bottomLeft, bottomRight, topLeft, topRight);
+    // Forward
+    yield return (topLeft, bottomLeft, topRight, bottomRight);
+    // Backward
+    yield return (topRight, bottomRight, topLeft, bottomLeft);
+  }
+
+  static bool CheckCandidate2(char[,] chars, (Point M1, Point M2, Point S1, Point S2) candidate)
+  {
+    var maxX = chars.GetLength(0);
+    var maxY = chars.GetLength(1);
+    var (m1, m2, s1, s2) = candidate;
+    return CheckPoint('M', m1) && CheckPoint('M', m2) && CheckPoint('S', s1) && CheckPoint('S', s2);
+
+    bool CheckPoint(char c, Point p) => p.X >= 0 && p.X < maxX && p.Y >= 0 && p.Y < maxY && chars[p.X, p.Y] == c;
   }
 }
