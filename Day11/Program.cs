f0ef906a5f297361b330c48a222e80b92cbd0c5d
Kristoffer Smed <mail@kristoffersmed.dk>
2024-12-11 20:48:04 +0100

    String based

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -12,47 +12,46 @@ var data = new Parser().Parse(input);
 var answer = new Solver2().Solve(data);
 Console.WriteLine($"# Answer Part 2: {answer}");
 Console.WriteLine($"# In: {total.Elapsed}");
-public class Parser : IParser<int[]>
+public class Parser : IParser<string[]>
 {
-  public int[] Parse(string[] values) => values.SelectMany(x => x.Split(' ')).Select(int.Parse).ToArray();
+  public string[] Parse(string[] values) => values.SelectMany(x => x.Split(' ')).ToArray();
 }
 
-public class Solver : ISolver<int[], int>
+public class Solver : ISolver<string[], int>
 {
   protected virtual int Blinks => 25;
-  public int Solve(int[] values)
+  public int Solve(string[] values)
   {
-    var stones = values.Select(x => (long)x);
+    var stones = values.ToList();
     for (var i = 0; i < Blinks; i++)
     {
       var sw = Stopwatch.StartNew();
       stones = Blink(stones).ToList();
-      Console.WriteLine($"Blink {i} {stones.Count()} in {sw.ElapsedMilliseconds}ms");
+      Console.WriteLine($"Blink {i} {stones.Count} in {sw.ElapsedMilliseconds}ms");
     }
     return stones.Count();
   }
 
-  static IEnumerable<long> Blink(IEnumerable<long> stones)
+  static IEnumerable<string> Blink(IEnumerable<string> stones)
   {
     foreach (var stone in stones)
     {
-      if (stone == 0)
+      if (stone == "0")
       {
-        yield return 1;
+        yield return "1";
         continue;
       }
 
-      var engraving = stone.ToString();
-      var digits = engraving.Length;
+      var digits = stone.Length;
       if (digits % 2 == 0)
       {
         var half = digits / 2;
-        yield return long.Parse(engraving[..half]);
-        yield return long.Parse(engraving[half..]);
+        yield return stone[..half];
+        yield return stone[half..].TrimStart('0');
         continue;
       }
 
-      yield return stone * 2024;
+      yield return (long.Parse(stone) * 2024).ToString();
     }
   }
 }
