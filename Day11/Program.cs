e22e2660e446480376d975ed90b24e85a8850998
Kristoffer Smed <mail@kristoffersmed.dk>
2024-12-11 20:42:40 +0100

    Time check

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -1,9 +1,17 @@
-﻿using Common;
+﻿using System.Diagnostics;
+
+using Common;
 
 var example= "125 17";
 
-Solving.Go(example, new Parser(), new Solver(), new Solver2());
+// Solving.Go(example, new Parser(), new Solver(), new Solver2());
 
+var total = Stopwatch.StartNew();
+var input = File.ReadAllLines("input.txt");
+var data = new Parser().Parse(input);
+var answer = new Solver2().Solve(data);
+Console.WriteLine($"# Answer Part 2: {answer}");
+Console.WriteLine($"# In: {total.Elapsed}");
 public class Parser : IParser<int[]>
 {
   public int[] Parse(string[] values) => values.SelectMany(x => x.Split(' ')).Select(int.Parse).ToArray();
@@ -17,7 +25,9 @@ public class Solver : ISolver<int[], int>
     var stones = values.Select(x => (long)x);
     for (var i = 0; i < Blinks; i++)
     {
-      stones = Blink(stones);
+      var sw = Stopwatch.StartNew();
+      stones = Blink(stones).ToList();
+      Console.WriteLine($"Blink {i} {stones.Count()} in {sw.ElapsedMilliseconds}ms");
     }
     return stones.Count();
   }
@@ -49,5 +59,5 @@ public class Solver : ISolver<int[], int>
 
 public class Solver2 : Solver
 {
-  protected override int Blinks => 75;
+  protected override int Blinks => 40;
 }
