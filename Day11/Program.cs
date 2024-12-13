121d8031fd70eaff86bba938c19f70ed789779a3
Kristoffer Smed <mail@kristoffersmed.dk>
2024-12-13 19:05:28 +0100

    draft - incomplete

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -192,6 +192,105 @@ public class SolverWithTree(int Blinks = 75) : ISolver<int[], int>
   }
 }
 
+public class Solver4(int Blinks) : ISolver<int[], int>
+{
+  //readonly Dictionary<long, Stone> treeCache = new();
+  Dictionary<long, CachedStone> countCache = new();
+
+  public int Solve(int[] data)
+  {
+    var count = 0;
+    foreach (var stone in data)
+    {
+      var sw = Stopwatch.StartNew();
+      var subCount = Count(stone, Blinks);
+      Console.WriteLine($"{stone}: Count: {subCount} in {sw.Elapsed}");
+      count += subCount;
+    }
+
+    return count;
+  }
+
+  int Count(int engraving, int blinks)
+  {
+    if (blinks == 0)
+      return 1;
+
+    CachedStone? cachedStone;
+    if (!countCache.TryGetValue(engraving, out cachedStone))
+    {
+      var s = new Stone(engraving);
+      cachedStone = new(s, [1], [s]);
+      countCache[engraving] = cachedStone;
+    }
+    else if (cachedStone.Counts.Count >= blinks)
+      return cachedStone.Counts[blinks];
+
+    var stones = cachedStone.MostBlinks;
+    for(var i = cachedStone.Counts.Count; i < blinks; i++)
+    {
+      var newStones = new List<Stone>();
+      foreach (var stone in stones)
+      {
+        var (engraving1, engraving2) = Blink(stone.Engraving);
+        if(!countCache.TryGetValue(engraving1, out var cachedStone1))
+        {
+          var s1 = new Stone(engraving1);
+          cachedStone1 = new(s1, [1], [s1]);
+          countCache[engraving1] = cachedStone1;
+        }
+        var 
+        var count2 = engraving2.HasValue ? Count(engraving2.Value, blinks - 1 - i) : 0;
+        count += count1 + count2;
+      }
+      var (engraving1, engraving2) = Blink(engraving);
+      var count1 = Count(engraving1, blinks - 1 - i);
+      var count2 = engraving2.HasValue ? Count(engraving2.Value, blinks - 1 - i) : 0;
+      count += count1 + count2;
+    }
+    ExpandTree(stone, Blinks - blinks, blinks);
+      if (stone.Depth < Blinks - blinks) throw new InvalidOperationException("Tree incomplete");
+      return stone;
+    }
+    while(stack.Count > 0)
+    {
+      var (current, blinks) = stack.Pop();
+      for(var i = 0; i < blinks; i++)
+      {
+        var next = current.Next ?? throw new InvalidOperationException("Tree incomplete");
+        if (next.Item2 != null)
+          stack.Push((next.Item2, blinks - 1-i));
+
+        current = next.Item1;
+      }
+
+      count++;
+    }
+
+    return count; 
+  }
+
+static (long, long?) Blink(long stone)
+{
+  if (stone == 0)
+  {
+    return (1, null);
+  }
+
+  var engraving = stone.ToString();
+  var digits = engraving.Length;
+  if (digits % 2 == 0)
+  {
+    var half = digits / 2;
+    return (long.Parse(engraving[..half]), long.Parse(engraving[half..]));
+  }
+
+  return (stone * 2024, null);
+}
+
+  record CachedStone(Stone Stone, List<int> Counts, List<Stone> MostBlinks);
+}
+
 record Stone(long Engraving)
 {
   public (Stone, Stone?)? Next { get; set; }
