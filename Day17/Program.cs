1a045cc38e1d45a57116d7486321ceca3943d496
Kristoffer Smed <mail@kristoffersmed.dk>
2024-12-18 20:10:26 +0100

    Day 17: Part 1

--- Day17/Program.cs
+++ Day17/Program.cs
@@ -2,6 +2,8 @@
 
 using Common;
 
+using ArgumentOutOfRangeException = System.ArgumentOutOfRangeException;
+
 var example =
   """
   Register A: 729
@@ -11,7 +13,7 @@ var example =
   Program: 0,1,5,4,3,0
   """;
 
-Solving.GoParse(example, new Parser());
+Solving.Go1(example, new Parser(), new Interpreter());
 
 class Parser : IParser<Input>
 {
@@ -37,7 +39,75 @@ class Parser : IParser<Input>
     }
     return new (a, b, c, program.ToArray());
   }
+}
 
+class Interpreter : ISolver<Input, string>
+{
+  public string Solve(Input data) => string.Join(",",Execute(data));
+
+  static IEnumerable<int> Execute(Input data)
+  {
+    var a =data.A;
+    var b = data.B;
+    var c = data.C;
+    var pc = 0;
+    while (pc < data.Program.Length)
+    {
+      var (instruction, comboOperand) = data.Program[pc];
+      switch (instruction)
+      {
+        case Instruction.Adv:
+          a /= (1 << GetOperandValue(comboOperand));
+          break;
+        case Instruction.Bxl:
+          b ^= (int)comboOperand;
+          break;
+        case Instruction.Bst:
+          b = GetOperandValue(comboOperand) % 8;
+          break;
+        case Instruction.Jnz:
+          if (a != 0)
+          {
+            if ((int)comboOperand % 2 == 1)
+              throw new InvalidOperationException("Odd jumping!");
+            pc = (int)comboOperand / 2;
+            continue;
+          }
+          break;
+        case Instruction.Bxc:
+          b ^= c;
+          break;
+        case Instruction.Out:
+          yield return GetOperandValue(comboOperand) % 8;
+          break;
+        case Instruction.Bdv:
+          b = a / (1 << GetOperandValue(comboOperand));
+          break;
+        case Instruction.Cdv:
+          c = a / (1 << GetOperandValue(comboOperand));
+          break;
+        default:
+          throw new InvalidOperationException("Invalid instruction");
+      }
+
+      pc++;
+    }
+
+    yield break;
+
+    int GetOperandValue(ComboOperand comboOperand) => comboOperand switch
+    {
+      ComboOperand.Zero => 0,
+      ComboOperand.One => 1,
+      ComboOperand.Two => 2,
+      ComboOperand.Three => 3,
+      ComboOperand.A => a,
+      ComboOperand.B => b,
+      ComboOperand.C => c,
+      ComboOperand.Reserved => throw new InvalidOperationException("Reserved operand"),
+      _ => throw new ArgumentOutOfRangeException(nameof(comboOperand), comboOperand, "Invalid operand")
+    };
+  }
 }
 
 enum ComboOperand
@@ -65,3 +135,5 @@ enum Instruction
 }
 
 record Input(int A, int B, int C, (Instruction Instruction, ComboOperand ComboOperand)[] Program);
+
+
